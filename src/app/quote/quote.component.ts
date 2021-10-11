2226de570908b50a1838e8b85df0c395dec65938
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-12 00:27:34 +0300

    Introduce the array of quotes to the quote component file

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    new Quote(1, '--context--', '......the quote.......', '<Author>', '<name>', new Date(2021, 8, 17), 0, 0),
+
+    new Quote(2, '--context--', '......the quote.......', '<Author>', '<name>', new Date(2021, 8, 23), 0, 0),
+
+    new Quote(3, '--context--', '......the quote.......', '<Author>', '<name>', new Date(2021, 8, 29), 0, 0),
+
+    new Quote(4, '--context--', '......the quote.......', '<Author>', '<name>', new Date(2021, 9, 11), 0, 0),
+
+    new Quote(5, '--context--', '......the quote.......', '<Author>', '<name>', new Date(2021, 9, 27), 0, 0),
+
+    new Quote(6, '--context--', '......the quote.......', '<Author>', '<name>', new Date(2021, 10, 2), 0, 0),
+  ];
 
   constructor() { }
 
