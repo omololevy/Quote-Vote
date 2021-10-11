a47fa3e35794a0bd965d3ca236e2e461b8931b53
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-12 01:22:35 +0300

    Create a pipe date count to evaluate the date difference of the quote

---
+++ src/app/date-count.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateCount'
+})
+export class DateCountPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
