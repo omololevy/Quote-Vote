d8759ab191f81475ae0c95d47a0119a74617434c
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-12 01:56:33 +0300

    Add the date computation function to calculate the date and time difference

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today : Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    let dateDifference = Math.abs(  todayWithNoTime - value  );
+    const secondsInDay = 86400;
+    let dateDifferenceSeconds = dateDifference *0.001;
+    let dateCounter = dateDifferenceSeconds/secondsInDay;
+    if (dateCounter>=1 && todayWithNoTime > value){
+      return dateCounter;
+    }
+    else{
+      return 0;
+    }
+    
   }
 
 }
