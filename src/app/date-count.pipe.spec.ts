2f82935d262571af5c055f728503563f77dee6e0
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-12 01:23:36 +0300

    Add the specification file alongside the date count pipe.

---
+++ src/app/date-count.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateCountPipe } from './date-count.pipe';
+
+describe('DateCountPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateCountPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
