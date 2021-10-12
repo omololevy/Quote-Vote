54980a980d11425e95e52934635d1fe2b6513766
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-12 12:19:19 +0300

    Create a function submit quote that adds the new quote to the already exixting quote. the order of the property maintained

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote(0, "", "", "", "", new Date, 0, 0);
+  @Output() addQuote = new EventEmitter<Quote>(); 
+
+   submitQuote(){
+  let me =  this.addQuote.emit(this.newQuote);
+  alert('Great! Thanks for adding a new Quote. Check below to see your amazing Quote!')
+  }
+  
+  
+
 
   constructor() { }
 
